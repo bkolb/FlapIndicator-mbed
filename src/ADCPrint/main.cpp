80292c9e3a8b8fb41ecfa89f200467beeda20ca3
Bernd Kolb <bernd.kolb@gmail.com>
2022-04-23 16:14:11 +0200

    reworked calibration

--- src/ADCPrint/main.cpp
+++ src/ADCPrint/main.cpp
@@ -1,5 +1,4 @@
 #include "mbed.h"
-#include "USBSerial.h"
 
 #include "PinNames.h"
 
@@ -8,31 +7,44 @@
 #include "DigitalOut.h"
 #include "UARTStateExchange.h"
 
-RavVal2LedThreshold raw2led {.LL_VAL	= 10000,
-					.LL_L5_VAL = 15000,
-					.L5_VAL	= 20000,
-					.L5_L4_VAL = 25000,
-					.L4_VAL	= 30000,
-					.L4_L3_VAL = 35000,
-					.L3_VAL	= 40000,
-					.L3_L2_VAL = 45000,
-					.L2_VAL	= 50000,
-					.L2_L1_VAL = 55000,
-					.L1_VAL	= 60000};	
+RavVal2LedThreshold raw2led {
+                                .LL_VAL         = 10900,
+                                .L5_VAL         = 21300,
+                                .L4_VAL         = 22800,
+                                .L3_VAL         = 28300,
+                                .L2_VAL         = 32400,
+                                .L1_VAL         = 37800,
+                                .OFFSET         = 700,
+								.OFFSET_L         = 1000,
+                        };
 
 ADCFlapPosReader reader(A0, raw2led);
 
-USBSerial pc;
+PinName varioButtonPin = A3;
+mbed::InterruptIn *varioBtn;
+
 //FlashIAP flash;
 
 #define COUNT 100
-#define CYCLES 4
+#define CYCLES 3
 
 #define cursup    "\033[A"
 
+bool buttonPressed;
+
+void handleVarioButtonPressed()
+{
+	buttonPressed = true;
+}
+
+
 void calcAvgValue(char flap, uint16_t *min, uint16_t *max) {
-	pc.printf("  * Set flap to %c and press key to continue\n", flap);
-	pc._getc();
+	buttonPressed = false;
+	printf("  * Set flap to %c and press vario button to continue\n", flap);
+	//pc._getc();
+	while(!buttonPressed){
+		ThisThread::sleep_for(50ms);
+	}
 
 	uint8_t ctr = 0;
 
@@ -45,20 +57,31 @@ void calcAvgValue(char flap, uint16_t *min, uint16_t *max) {
 			*max = rawValue;
 		}
 
-		pc.printf("\r    ==> %u", ctr);
-		pc.sync();
+		printf("\r    ==> %u (%u)", ctr, rawValue);
+		fflush(stdout);
 		ctr++;
 		ThisThread::sleep_for(50ms);
 	}
-	pc.printf(cursup);
-	pc.printf(cursup);
-	pc.sync();
+	//printf(cursup);
+	printf(cursup);
+	fflush(stdout);
+}
+
+uint16_t avgValue(uint16_t min, uint16_t max) {
+	uint16_t halfDiff = (max-min)/2;
+	uint16_t res = max - halfDiff;
+
+	return res;
 }
 
 
 // main() runs in its own thread in the OS
 int main()
 {
+
+	varioBtn = new mbed::InterruptIn(varioButtonPin);
+	varioBtn->mode(PullUp);
+	varioBtn->fall(mbed::callback(&handleVarioButtonPressed));
 	/*
 	while (true) {
 		uint16_t raw = reader.readRawVal();
@@ -85,13 +108,13 @@ int main()
 	uint16_t flap_1min = 65000;
 	uint16_t flap_1max = 0;
 
-	pc.printf("=== Starting Calibration ===\n");
+	printf("=== Starting Calibration ===\n");
 
 	uint8_t cycle = 0;
 
 	while(cycle<CYCLES) {
 		cycle++;
-		pc.printf("= Cycle %c of %c =\n",cycle, CYCLES);
+		printf("= Cycle %c of %c =\n",cycle, CYCLES);
 		calcAvgValue('L', &flap_Lmin, &flap_Lmax);
 		calcAvgValue('5', &flap_5min, &flap_5max);
 		calcAvgValue('4', &flap_4min, &flap_4max);
@@ -100,23 +123,22 @@ int main()
 		calcAvgValue('1', &flap_1min, &flap_1max);
 	}
 
-	pc.printf("\n");
-	pc.printf("=== Calibration Finished ===\n");
-	pc.printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("\n");
+	printf("=== Calibration Finished ===\n");
+	printf("\n");
 
-	pc.printf("RavVal2LedThreshold raw2led {\n");
-	pc.printf("				.LL_VAL		= %u,\n", flap_5min-50);
-	pc.printf("				.LL_L5_VAL 	= %u,\n", flap_5min-10);
-	pc.printf("				.L5_VAL		= %u,\n", flap_5max+50);
-	pc.printf("				.L5_L4_VAL 	= %u,\n", flap_4min-50);
-	pc.printf("				.L4_VAL		= %u,\n", flap_4max+50);
-	pc.printf("				.L4_L3_VAL 	= %u,\n", flap_3min-50);
-	pc.printf("				.L3_VAL		= %u,\n", flap_3max+50);
-	pc.printf("				.L3_L2_VAL 	= %u,\n", flap_2min-50);
-	pc.printf("				.L2_VAL		= %u,\n", flap_2max+50);
-	pc.printf("				.L2_L1_VAL 	= %u,\n", flap_1min-50);
-	pc.printf("				.L1_VAL		= %u\n" , flap_1max+50);
-	pc.printf("			};\n"									);
+	printf("RavVal2LedThreshold raw2led {\n");
+	printf("				.LL_VAL		= %u,\n", avgValue(flap_Lmin, flap_Lmax));
+	printf("				.L5_VAL		= %u,\n", avgValue(flap_5min, flap_5max));
+	printf("				.L4_VAL		= %u,\n", avgValue(flap_4min, flap_4max));
+	printf("				.L3_VAL		= %u,\n", avgValue(flap_3min, flap_3max));
+	printf("				.L2_VAL		= %u,\n", avgValue(flap_2min, flap_2max));
+	printf("				.L1_VAL		= %u,\n", avgValue(flap_1min, flap_1max));
+	printf("				.OFFSET		= 700,\n");
+	printf("				.OFFSET_L		= 1000;\n");
+	printf("			};\n"									);
 
 
 	return 0;
